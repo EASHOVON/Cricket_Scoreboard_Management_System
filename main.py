aafcdcf24714c1a359f944eb1ff1475b3d58a7f4
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-11-23 17:12:29 +0600

    Made a class of Player and Test done

--- main.py
+++ main.py
@@ -9,6 +9,29 @@ class Team:
         self.playersListOfObject.append(player)
 
 
+# Making class Player
+class Player:
+    def __init__(self,name,teamObj) -> None:
+        self.playerName = name
+        self.strikeRate = 0.0
+        self.runBat = 0
+        self.ballUsed = 0
+        self.fours = 0
+        self.sixes = 0
+        self.runBowl = 0
+        self.wicketsTaken = 0
+        self.ballsBowled = 0
+        teamObj.playersListOfObject.append(self)
+
+    def __repr__(self) -> str:
+        return f"Name: {self.playerName}"
+
 bangladesh = Team("Bangladesh")
 india = Team("India")
+
+tamim = Player("Tamim Iqbal",bangladesh)
+sakib = Player("Sakib Al Hasan",bangladesh)
+mustafiz = Player("Mustafizur Rahman",bangladesh)
+
+print(bangladesh.playersListOfObject)
 
